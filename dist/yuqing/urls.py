2477b46087d991afef5395b32cbf9a8495ca0c47
zhmin <zhmin@shendu.info>
2015-03-03 15:26:14 +0800

    add vendor static file support

--- dist/yuqing/urls.py
+++ dist/yuqing/urls.py
@@ -1,12 +1,17 @@
+import os
+
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from django.views.static import serve
 
+from django.conf import settings
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'yuqing.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^vendor/(?P<path>.*)$', serve,  {'document_root': os.path.join(settings.BASE_DIR, 'vendor')}),
 )
 
 urlpatterns += patterns('yqj.views',
