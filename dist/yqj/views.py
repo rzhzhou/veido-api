881cc213d1b194d70dc7c8eea32e235a578c2942
zhmin <zhmin@shendu.info>
2015-03-04 17:51:44 +0800

    add dashboard number

--- dist/yqj/views.py
+++ dist/yqj/views.py
@@ -6,7 +6,11 @@ def index_view(request):
     user = {'name': 'wuhan', 'company': u'武汉质监局'}
     categories = [{'name': u'微博', 'id': '98672345'}, {'name': u'文章', 'id': '52345609'}]
     locations = [{'name': u'武昌', 'id': '98672345'}, {'name': u'汉口', 'id': '52345609'}]
-    return render_to_response("dashboard/dashboard.html", {'user': user, 'categories': categories, 'locations': locations})
+    news = {'number': 67}
+    weibo = {'number': 167}
+    weixin = {'number': 53}
+    event = {'number': 29}
+    return render_to_response("dashboard/dashboard.html", {'user': user, 'categories': categories, 'locations': locations, 'news': news, 'weibo': weibo, 'weixin': weixin, 'event': event})
 
 def category_view(request, ctg_id):
     return render_to_response('category/category.html')
