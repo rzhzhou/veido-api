cb29f192863a97c48763209746a6de1eefb36bfd
Liu Fei <liuf@shendu.info>
2015-03-04 16:58:08 +0800

    update adminlte

--- update.sh
+++ update.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+### vendor ###
+
 # remove vendor directory
 rm -rf dist/vendor
 
@@ -11,19 +13,6 @@ cp -r bower_components/admin-lte/bootstrap dist/vendor/bootstrap
 
 # copy dist of adminlte to dist/vendor
 cp -r bower_components/admin-lte/dist dist/vendor/adminlte
-
-# remove adminlte & bootstrap from src/less
-rm -rf src/less/bootstrap
-rm -rf src/less/adminlte
-
-# copy build/bootstrap-less to src/less/bootstrap
-cp -r bower_components/admin-lte/build/bootstrap-less src/less/bootstrap
-
-# copy build/less to src/less/adminlte
-cp -r bower_components/admin-lte/build/less src/less/adminlte
-
-# remove echarts from dist/vendor
-rm -rf dist/vendor/echarts
 
 # copy dist of echarts to dist/vendor
 cp -r bower_components/echarts/build/dist dist/vendor/echarts
@@ -44,5 +33,17 @@ mkdir dist/vendor/ionicons
 # copy css and fonts to dist/vendor/ionicons
 cp -r bower_components/ionicons/css dist/vendor/ionicons/css
 cp -r bower_components/ionicons/fonts dist/vendor/ionicons/fonts
+
+### src ###
+
+# remove adminlte & bootstrap from src/less
+rm -rf src/less/bootstrap
+rm -rf src/less/adminlte
+
+# copy build/bootstrap-less to src/less/bootstrap
+cp -r bower_components/admin-lte/build/bootstrap-less src/less/bootstrap
+
+# copy build/less to src/less/adminlte
+cp -r bower_components/admin-lte/build/less src/less/adminlte
 
 echo "finish update"
