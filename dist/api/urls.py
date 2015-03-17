fe63f7b155f035fd41884abacb86e13e14d7e5c4
chzhao <zhaochen@shendu.info>
2015-03-17 17:13:42 +0800

    add news api

--- dist/api/urls.py
+++ dist/api/urls.py
@@ -1,10 +1,11 @@
 from django.conf.urls import patterns, include, url
-from api.views import ArticleTableView, NewsTableView
+from api.views import ArticleTableView, NewsTableView, LocationTableView
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'yuqing.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^category/(\d+)/$', ArticleTableView.as_view()),
+    url(r'^location/(\d+)/$', LocationTableView.as_view()),
     url(r'^news/$', NewsTableView.as_view()),
 )
