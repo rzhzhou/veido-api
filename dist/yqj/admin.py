adabc084a078cf277a43ef49782d88cbd1b6eb99
guoxx <guoxx@shendu.info>
2015-05-08 17:43:46 +0800

    alter admin

--- dist/yqj/admin.py
+++ dist/yqj/admin.py
@@ -2,8 +2,7 @@
 from django.contrib import admin
 from django import forms
 from django.contrib import messages
-from datetime import datetime, timedelta
-from yqj.mongoconnect import MongodbQuerApi
+from yqj.mongoconnect import CrawlerTask
 from models import WeixinPublisher, WeiboPublisher, ArticlePublisher,\
                    ArticleCategory, Group, User, Article, Topic, Custom,\
                    Keyword, Area
@@ -39,64 +38,16 @@ class KeywordAdmin(admin.ModelAdmin):
         obj.save()
 
 class TopicAdmin(admin.ModelAdmin):
+    fields = ('title', 'abstract', 'source', 'area', 'keywords')
     list_display = ('title', 'source', 'area')
     list_editable = ('source', 'area',)
     list_filter = ('source',)
     search_fields = ('title', 'source')
 
-class CrawlerTask(object):
+    def save_model(self,request, obj, form, change):
+        CrawlerTask(obj.title, 'zjld', u"事件").type_task()
+        obj.save()
 
-    def __init__(self, key, mongo_task, task_type):
-        self.key = key
-        self.mongo_task = mongo_task
-        self.task_type = task_type
-
-    def insert_task(self, data):
-        crawler_conf = {
-            "type" : data.get('type',''),
-            "status" : data.get('status', 0),
-            "priority" : data.get('priority',3),
-            "interval" : data.get('interval', 7200),
-            "update_time" : datetime.utcnow(),
-            "lastrun": datetime.utcnow(),
-            "nextrun": datetime.utcnow() - timedelta(days=2),
-            "crtetime": datetime.utcnow(),
-            "timeout": 3600,
-            "key": self.key,
-            "data" : {
-                "source_type" : self.task_type,
-                "source" : data.get('source', '')}
-        }
-
-        if not MongodbQuerApi(self.mongo_task).find_one({'type':data.get('type',''),
-                    'key': self.key}):
-            MongodbQuerApi(self.mongo_task).save(crawler_conf)
-
-    def type_task(self):
-        types = {
-            "baidu": "zjld.baidu.newstitle",
-            "weibo": "zjld.weibo.newstitle",
-            "sogou": "zjld.sogou.keywords",
-        }
-        weibodata = {
-            "interval": 21600,
-            "type": types.get('weibo'),
-            "source": 'weibo'
-        }
-
-        weibo = self.insert_task(weibodata)
-
-        baidudata = {
-            "type": types.get('baidu'),
-            "source": 'baidu',
-        }
-        baidu = self.insert_task(baidudata)
-
-        weixindata = {
-            "type": types.get('sogou'),
-            "source": 'sogou'
-        }
-        weixin = self.insert_task(weixindata)
 
 # Register your models here.
 
