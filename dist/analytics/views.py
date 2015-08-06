cadd37d678e1f80c9cee56c871323c71d7cb6482
mfwu <mfwu@shendu.info>
2015-08-05 21:15:06 -0700

    complete the chart

--- dist/analytics/views.py
+++ dist/analytics/views.py
@@ -29,7 +29,52 @@ class DispatchView(APIView):
         return Response({'total': total, 'risk': risk})
 
     def chart_type(self, start, end):
-        return Response({})
+        try:
+            article = Article.objects.filter(pubtime__range=(start,end)).count()
+            weixin = Weixin.objects.filter(pubtime__range=(start,end)).count()
+            weibo = Weibo.objects.filter(pubtime__range=(start,end)).count()
+            return Response({'news': article, 'weixin': weixin, 'weibo': weibo})
+        except:
+            return Response({}) 
+
+    def chart_emotion(self, start, end): 
+        try:
+            positive = Article.objects.filter(pubtime__range=(start,end),
+                feeling_factor__gte=0.6).count()
+
+            normal = Article.objects.filter(pubtime__range=(start,end),
+                feeling_factor__gte=0.5, feeling_factor__lt=0.6).count()
+
+            negative = Article.objects.filter(pubtime__range=(start,end), feeling_factor__lt=0.5).count()
+
+            return Response({'positive':positive, 'normal': normal, 'negative': negative})
+        except:
+            return Response({})
+
+    def chart_weibo(self, start, end):
+        try:
+            pro_area = Area.objects.filter(level=2)
+            pro_id = []
+            for item in pro_area:
+                pro_id.append(item.id)
+            provice_count = []
+            for i in pro_id:
+                city_area = Area.objects.filter(parent_id=i)
+                city_id = []
+                for c in city_area:
+                    city_id.append(c.id)
+                one_pro = Area.objects.filter(Q(parent_id__in=city_id)|Q(parent_id=i)|Q(id=i))
+                one_pro_id = []
+                for items in one_pro:
+                    one_pro_id.append(items.id)
+                count = Weibo.objects.filter(area__in=one_pro, pubtime__range=(start,end)).count()
+                name =  Area.objects.get(id=i).name
+                provice_count.append({'name': name, 'count': count})
+            sort_result = sorted(provice_count, key=lambda x:x['count'], reverse=True)[:6]
+
+            return Response({'provice_count':provice_count, 'sort_result': sort_result})
+        except:
+            return Response({})
 
 
 class LineTableView():
@@ -60,59 +105,6 @@ class LineTableView():
         "weibo_data": weibo_data,
         "total_data": total_data
         })
-
-
-class PieTypeTableView():
-    def get(self, request):
-        start = datetime.strptime(request.GET['start'], '%Y-%m-%d')
-        end = datetime.strptime(request.GET['end'], '%Y-%m-%d')+timedelta(days=1)
-        news = Article.objects.filter(pubtime__range=(start,end)).count()
-        weixin = WeiXin.objects.filter(pubtime__range=(start,end)).count()
-        weibo = Weibo.objects.filter(pubtime__range=(start,end)).count()
-
-        return Response({'news': article, 'weixin': weixin, 'weibo': weibo})
-
-class PieFeelingTableView():
-    def get(self, request, id):
-        start = datetime.strptime(request.GET['start'], '%Y-%m-%d')
-        end = datetime.strptime(request.GET['end'], '%Y-%m-%d')+timedelta(days=1)
-        positive = Article.objects.filter(pubtime__range=(start,end),
-            feeling_factor__gte=0.6).count()
-
-        normal = Article.objects.filter(pubtime__range=(start,end),
-            feeling_factor__gte=0.5, feeling_factor__lt=0.6).count()
-
-        negative = Article.objects.filter(pubtime__range=(start,end), feeling_factor__lt=0.5).count()
-
-        return Response({'positive':positive, 'normal': normal, 'negative': negative})
-
-
-class PieAreaTableView():
-    def get(self, request, id):
-    #SELECT id FROM yqj.`area` WHERE parent_id IN (SELECT id FROM yqj.`area` WHERE parent_id=2) OR parent_id=2 OR id =2
-        start = datetime.strptime(request.GET['start'], '%Y-%m-%d')
-        end = datetime.strptime(request.GET['end'], '%Y-%m-%d')+timedelta(days=1)
-        pro_area = Area.objects.filter(level=2)
-        pro_id = []
-        for item in pro_area:
-            pro_id.append(item.id)
-
-        provice_count = []
-        for i in pro_id:
-            city_area = Area.objects.filter(parent_id=i)
-            city_id = []
-            for c in city_area:
-                city_id.append(c.id)
-            one_pro = Area.objects.filter(Q(parent_id__in=city_id)|Q(parent_id=i)|Q(id=i))
-            one_pro_id = []
-            for items in one_pro:
-                one_pro_id.append(items.id)
-            count = Weibo.objects.filter(area__in=one_pro, pubtime__range=(start,end)).count()
-            name =  Area.objects.get(id=i).name
-            provice_count.append({'name': name, 'count': count})
-        sort_result = sorted(provice_count, key=lambda x:x['count'], reverse=True)[:6]
-
-        return Response({'provice_count':provice_count, 'sort_result': sort_result})
 
 
 class AnalyticsView(BaseView):
