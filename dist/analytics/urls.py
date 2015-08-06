01afe8b3ec646767616601eca386900be9273459
sli <sli@shendu.info>
2015-08-06 10:12:17 +0800

    add chartline

--- dist/analytics/urls.py
+++ dist/analytics/urls.py
@@ -1,9 +1,10 @@
 from django.conf.urls import patterns, include, url
+from views import LineTableView
 
 
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'yuqing.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-
+    url(r'^analytics/charts/(?P<id>\d+)/$', LineTableView.as_view()),
 )
--- dist/analytics/views.py
+++ dist/analytics/views.py
@@ -1,8 +1,13 @@
 # -*- coding: utf-8 -*-
 from django.shortcuts import render
 from rest_framework.views import APIView
-
 from yqj.views import BaseView
+from analytics.api_tools import analytics_data
+from datetime import date, datetime, timedelta
+from dateutil.relativedelta import relativedelta
+from django.utils.timezone import utc
+from yqj.models import Article, Weixin, Weibo
+from rest_framework.response import Response
 
 
 class TableAPIView(APIView):
@@ -11,8 +16,26 @@ class TableAPIView(APIView):
 
 
 class LineTableView(TableAPIView):
-    def get(self, request ):
-        pass
+    """
+    {
+        "date": ["07-30", "07-31", "08-01", "08-02", "08-03", "08-04", "08-05"],
+        "news_data": [491, 363, 164, 116, 331, 381, 168], 
+    }
+    """
+    def get(self, request, id):
+        start = datetime.strptime(request.GET['start'], "%Y-%m-%d")
+        end = datetime.strptime(request.GET['end'], "%Y-%m-%d")
+        date_range = end - start
+        date = [(start + timedelta(days=x)).date() for x in xrange(date_range.days)]
+        news_data = [Article.objects.filter(pubtime__gte=date[x], pubtime__lt=date[x] + timedelta(days=1)).count() for x in xrange(date_range.days)]
+        weixin_data = [Weixin.objects.filter(pubtime__gte=date[x], pubtime__lt=date[x] + timedelta(days=1)).count() for x in xrange(date_range.days)]
+        weibo_data = [Weibo.objects.filter(pubtime__gte=date[x], pubtime__lt=date[x] + timedelta(days=1)).count() for x in xrange(date_range.days)]
+        return Response({
+        "date": date,
+        "news_data": news_data, 
+        "weixin_data": weixin_data,
+        "weibo_data": weibo_data,
+        })
 
 
 class PieTypeTableView(TableAPIView):
