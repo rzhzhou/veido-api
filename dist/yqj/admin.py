af6c9e64774642c58b326e895174689439312eee
sli <sli@shendu.info>
2015-08-14 21:43:00 +0800

    event fix admin

--- dist/yqj/admin.py
+++ dist/yqj/admin.py
@@ -128,10 +128,10 @@ class CustomKeywordAdmin(admin.ModelAdmin):
 
 
 class TopicAdmin(admin.ModelAdmin):
-    fields = ('title', 'abstract', 'source', 'area', 'keywords', 'pubtime',)
-    list_display = ('title', 'source',  'pubtime', 'area',)
-    list_editable = ('source', 'area', 'pubtime',)
-    list_filter = ('source','pubtime',)
+    fields = ('title', 'abstract', 'source', 'area', 'keywords')
+    list_display = ('title', 'source', 'area',)
+    list_editable = ('source', 'area')
+    list_filter = ('source',)
     search_fields = ('title', 'source')
     #
     def save_model(self, request, obj, form, change):
