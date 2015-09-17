d90a54ed31089c28f17c0c73238ad8f988d560f6
Liu Fei <liuf@shendu.info>
2015-09-17 17:36:43 +0800

    fix risk detail

--- dist/static/js/app.js
+++ dist/static/js/app.js
@@ -1756,8 +1756,24 @@ App.page.riskDetail = function (module, path, type, id) {
   module.collect(type, id);
   module.line(path, type);
   module.pie(path, type);
-  module.table(module, path);
-  module.sns(module, path, type);
+  
+  module.detail({
+    path: path,
+    feature: 'news',
+    container: '#news > tbody'
+  });
+
+  module.detail({
+    path: path,
+    feature: 'weixin',
+    container: '#weixin'
+  });
+
+  module.detail({
+    path: path,
+    feature: 'weibo',
+    container: '#weibo'
+  });
 };
 
 App.page.analyticsDetail = function (module, path) {
