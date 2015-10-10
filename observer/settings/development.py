5bab24de8725bb53302ccbd377cd8c5d8471b7ea
Liuxi Wu <lxwu@shendu.info>
2015-10-10 17:21:10 +0800

    static dirs

--- observer/settings/development.py
+++ observer/settings/development.py
@@ -188,7 +188,7 @@ DATABASE_ROUTERS = ['corpus.router.MyDB2Router',]
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, "../static"),
+    os.path.join(BASE_DIR, "../static/build/"),
 )
 
 
