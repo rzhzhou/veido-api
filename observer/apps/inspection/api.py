3ef1d12b1225de3d54ce3a359b890b4a02a7cf29
mfwu <mfwu@shendu.info>
2015-10-14 03:22:12 -0700

    inspection api

--- observer/apps/inspection/api.py
+++ observer/apps/inspection/api.py
@@ -13,32 +13,12 @@ from observer.apps.base.models import Inspection
 
 class InspectionTableView(BaseAPIView):
     def get(self, request):
-        result = []
-        news = Inspection.objects.exclude(qualitied__lt=0).order_by('-pubtime').all()
-
-        for item in news:
-            title = self.title_html(item.url, item.name, item.id, 'inspection')
-            quality = str(int(item.qualitied*100)) + '%'
-            tz  = pytz.timezone(settings.TIME_ZONE)
-            timel = item.pubtime.astimezone(tz)
-
-            one_record = [item.product, title, quality, item.source, timel.strftime('%Y-%m-%d')]
-            result.append(one_record)
-
-        return Response({"inspection": result})
-
-    def inspection_to_json(self, items):
-        result = []
-        for data in items:
-            item = {}
-            item['title'] = data.name
-            item['source'] = data.source
-            item['category'] = data.product
-            item['quality'] = str(data.qualitied * 100)[:4] + "%"
-            item['time'] = data.pubtime.replace(tzinfo=None).strftime('%Y-%m-%d')
-            item['source'] = data.source
-            result.append(item)
-        return result
+        container = self.requestContainer(limit_list=settings.EVENT_PAGE_LIMIT)
+        items = Inspection.objects.exclude(qualitied__lt=0).order_by('-pubtime')
+        datas = self.paging(items, container['limit'], container['page'])
+        result = self.inspection_to_json(datas['items'])
+        html_string = render_to_string('inspection/list_tpl.html', {'inspection_list':  result})
+        return Response({'total': datas['total_number'], 'html': html_string})
 
 
 class InspectionLocalView(BaseAPIView):
