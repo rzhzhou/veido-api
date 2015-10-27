7b73272ed6ab4cd105fc99cdee14822e6b1b4931
mfwu <mfwu@shendu.info>
2015-10-27 00:56:26 -0700

    rm cookie

--- observer/utils/cache/catch.py
+++ observer/utils/cache/catch.py
@@ -1,5 +1,5 @@
 import sys
-root_mod = '/home/feng/Project/observer/app'
+root_mod = '/home/feng/Project/observer/api'
 sys.path.append(root_mod)
 import os
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "observer.settings.development")
@@ -25,13 +25,6 @@ class BaseCatch(BaseJob):
             'User-Agent':'Mozilla/5.0 (Windows; U; Windows NT 6.1; en-US; rv:1.9.1.6) Gecko/20091201 Firefox/3.5.6',
         }
 
-        res = requests.post(
-            url = '%s/api/login/' % url_cfg,
-            data = {
-            'username': datas['username'],
-            'password': datas['password']}
-            )
-        headers['Cookie'] = res.headers['set-cookie']
         result = requests.get(url=url_cache, headers=headers)
         RedisQueryApi().hset(datas['hset_name'], datas['hset_key'], str(result.text))
 
