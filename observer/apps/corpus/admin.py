953aaf04d120b3b4c8e73cec7cca6a7c5710335c
Liuxi Wu <lxwu@shendu.info>
2015-12-07 11:41:38 +0800

    automatic extracting stype for sync

--- observer/apps/corpus/admin.py
+++ observer/apps/corpus/admin.py
@@ -1,4 +1,6 @@
 # -*- coding: utf-8 -*-
+import os
+
 from django.contrib import admin
 
 from observer.apps.corpus.models import Corpus
@@ -11,19 +13,23 @@ class CorpusAdmin(admin.ModelAdmin):
     list_filter = ('industry', 'riskword')
     search_fields = ('industry', 'riskword')
 
+    def __init__(self, *args, **kwargs):
+        super(CorpusAdmin, self).__init__(*args, **kwargs)
+        self.stype = os.path.basename(os.path.dirname(__file__))
+
     def save_model(self, request, obj, form, change):
         if not change:
             CrawlerTask(obj.uuid, obj.industry.name, 
-                obj.riskword.name, 'riskmonitor', u'关键词').build()
+                obj.riskword.name, 'riskmonitor', self.stype).build()
         else:
             CrawlerTask(obj.uuid, obj.industry.name, 
-                obj.riskword.name, 'riskmonitor', u'关键词').update(obj.uuid)
+                obj.riskword.name, 'riskmonitor', self.stype).update(obj.uuid)
 
         obj.save()
 
     def delete_model(self, request, obj):
         CrawlerTask(obj.uuid, obj.industry.name, 
-                obj.riskword.name, 'riskmonitor', u'关键词').remove(obj.uuid)
+                obj.riskword.name, 'riskmonitor', self.stype).remove(obj.uuid)
         obj.delete()
 
 
