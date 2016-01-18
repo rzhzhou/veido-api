fac9fa19ee46c09f84a497c3525104ddd8399842
sli <sli@shendu.info>
2016-01-18 14:39:13 +0800

    add test case

--- observer/apps/riskmonitor/tests.py
+++ observer/apps/riskmonitor/tests.py
@@ -1,3 +1,13 @@
+import os
+import sys
+import django
+
 from django.test import TestCase
 
 # Create your tests here.
+def test_tools():
+	reload(sys)
+	root_mod = '/home/sli/workweb/api/'
+	sys.path.append(root_mod)
+	os.environ.setdefault("DJANGO_SETTINGS_MODULE", "observer.settings.development");
+	django.setup()
