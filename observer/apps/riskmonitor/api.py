ed602e8b2e73f5e93354ed5c460b503069eb36e0
sli <sli@shendu.info>
2016-01-19 01:10:40 -0500

    repair test case

--- observer/apps/riskmonitor/api.py
+++ observer/apps/riskmonitor/api.py
@@ -2,5 +2,4 @@
 from tests import test_tools
 test_tools()
 
-from observer.apps.base.models import Area
-print Area.objects.filter(id=3)
+
--- observer/apps/riskmonitor/tests.py
+++ observer/apps/riskmonitor/tests.py
@@ -4,10 +4,12 @@ import django
 
 from django.test import TestCase
 
+BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+PROJECT_ROOT = os.path.abspath(os.path.dirname(BASE_DIR+'/../../..'))
+
 # Create your tests here.
 def test_tools():
 	reload(sys)
-	root_mod = '/home/code/gitlab/api/'
-	sys.path.append(root_mod)
+	sys.path.append(PROJECT_ROOT)
 	os.environ.setdefault("DJANGO_SETTINGS_MODULE", "observer.settings.development");
 	django.setup()
