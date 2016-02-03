36c3c17002ee066b65c67022b1c86a8e806a5845
sli <sli@shendu.info>
2016-02-02 21:31:52 -0500

    add framework

--- observer/apps/riskmonitor/api.py
+++ observer/apps/riskmonitor/api.py
@@ -2,4 +2,34 @@
 from tests import test_tools
 test_tools()
 
+from rest_framework.views import APIView
 
+from observer.apps.base.views import BaseTemplateView
+from businesslogic.detail import *
+from businesslogic.enterprise_rank import *
+from businesslogic.homepage import *
+from businesslogic.industry_track import *
+from businesslogic.statistic import *
+
+class DispatchView(APIView, BaseTemplateView):
+	def get(self, request, id):
+		container = self.requesthead()
+		type = contarner['type']
+		func = getattr(globals()['DispatchView'](), type)
+		return func(request, id)
+
+	def homepage(self, request, id):
+	    pass
+
+	def industry_track(self, request, id):
+	    pass
+
+	def enterprise_rank(self, request, id):
+	    pass
+
+	def statistic(self, request, id):
+	    pass
+
+	def detail(self, request, id):
+	    pass
+DispatchView().get('homepage', 3)
