c420ba0f1bf1dba62d867ec6a80e873f4a9de29b
mfwu <mfwu@shendu.info>
2016-03-03 00:47:34 -0800

    riskmonitor test

--- observer/apps/riskmonitor/tests.py
+++ observer/apps/riskmonitor/tests.py
@@ -2,7 +2,11 @@ import os
 import sys
 import django
 
+from django.test import Client
 from django.test import TestCase
+
+from observer.apps.base.tests import BaseTestCase
+
 
 PROJECT_ROOT = os.path.abspath(os.path.dirname(os.getcwd()+'/../../../'))
 def test_tools():
@@ -13,3 +17,12 @@ def test_tools():
 
 
 # test_tools()
+
+
+class APITest(BaseTestCase):
+
+    def test_home_page_view(self):
+        token = self.get_token()
+        csrf_client = Client(enforce_csrf_checks=True)
+        response = csrf_client.get('/api/dashboards', {}, HTTP_AUTHORIZATION='Bearer {}'.format(token))
+        self.assertEqual(response.status_code, 301)
