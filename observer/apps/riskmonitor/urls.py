225787b2fa43d73c0c4f9e5a88bd5551fc69d8c8
lxwu <lxwu@shendu.info>
2016-03-10 22:41:06 -0500

    refactor views, urls conf

--- observer/apps/riskmonitor/urls.py
+++ observer/apps/riskmonitor/urls.py
@@ -1,16 +1,17 @@
 from django.conf.urls import url
 
 from observer.apps.riskmonitor.views import (
-    HomePageView, IndustryTrackView, EnterpriseRankView, StatisticView,
-    DetailNewsView, SpeciesView, StatisticExportView)
+    HomePageView, IndustryList, IndustryDetail, NewsList, NewsDetail,
+    EnterpriseList, Analytics, AnalyticsExport)
 
 
 urlpatterns = [
     url(r'^dashboard$', HomePageView.as_view()),
-    url(r'^species$', SpeciesView.as_view()),
-    url(r'^industry/(?P<pk>[0-9]+)$', IndustryTrackView.as_view()),
-    url(r'^enterprise$', EnterpriseRankView.as_view()),
-    url(r'^statistical$', StatisticView.as_view()),
-    url(r'^statistical/export$', StatisticExportView.as_view()),
-    url(r'^news$', DetailNewsView.as_view()),
+    url(r'^industry$', IndustryList.as_view()),
+    url(r'^industry/(?P<pk>[0-9]+)$', IndustryDetail.as_view()),
+    url(r'^news$', NewsList.as_view()),
+    url(r'^news/(?P<pk>[0-9]+)$', NewsDetail.as_view()),
+    url(r'^enterprise$', EnterpriseList.as_view()),
+    url(r'^analytics$', Analytics.as_view()),
+    url(r'^analytics/export$', AnalyticsExport.as_view()),
 ]
