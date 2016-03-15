7afea45248f6cdff154a675388e780a0a6067a2b
sli <sli@shendu.info>
2016-03-15 04:05:46 -0400

    add industries test case

--- observer/apps/base/tests.py
+++ observer/apps/base/tests.py
@@ -1,26 +1,19 @@
 # -*- coding: utf-8 -*-
-from django.test import TestCase
-from django.test import Client
-from django.contrib.auth.models import User
+from django.contrib.auth.models import User, Group, Permission
 from rest_framework.test import APIClient
 
-from observer.apps.base.models import Category, Area
 
+class BaseTestCase():
 
-class BaseTestCase(TestCase):
-
-    def setUp(self):
-        self.email = 'admin@shendu.info'
-        self.username = 'wuhan'
-        self.password = 'wuhan'
-        self.user = User.objects.create_user(self.username, self.email, self.password)
-        self.data = {'username':self.username, 'password': self.password}
-        category = Category(name= u'质监热点')
-        category.save()
-        area = Area(name=u'全国', level=1)
-        area.save()
-
-    def get_token(self):
+    def get_client(self):
+        email = 'admin@shendu.info'
+        username = 'wuhan'
+        password = 'wuhan'
+        if User.objects.filter(username=username).exists() is not None:
+            User.objects.create_user(username, email, password)
+        udata = {'username': 'wuhan', 'password': 'wuhan'}
         client = APIClient(enforce_csrf_checks=True)
-        response = client.post('/api/token-auth', self.data, format='json')
-        return response.data['token']
+        response = client.post('/api/token-auth', udata)
+        token = response.data['token']
+        client.credentials(HTTP_AUTHORIZATION='Bearer  ' + token)
+        return client
--- observer/apps/riskmonitor/tests.py
+++ observer/apps/riskmonitor/tests.py
@@ -1,29 +1,14 @@
-import os
-import sys
-import django
-
 from django.test import Client
 from django.test import TestCase
+from rest_framework.test import APITestCase
+from rest_framework import status
 
 from observer.apps.base.tests import BaseTestCase
 
 
-PROJECT_ROOT = os.path.abspath(os.path.dirname(os.getcwd()+'/..'))
-print PROJECT_ROOT
-def test_tools():
-	reload(sys)
-	sys.path.append(PROJECT_ROOT)
-	os.environ.setdefault("DJANGO_SETTINGS_MODULE", "observer.settings.development");
-	django.setup()
-
-
-# test_tools()
-
-
-class APITest(BaseTestCase):
+class APITest(BaseTestCase, APITestCase):
 
     def test_home_page_view(self):
-        token = self.get_token()
-        csrf_client = Client(enforce_csrf_checks=True)
-        response = csrf_client.get('/api/dashboards', {}, HTTP_AUTHORIZATION='Bearer {}'.format(token))
-        self.assertEqual(response.status_code, 301)
+        client = self.get_client()
+        response = client.get('/api/industries')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
