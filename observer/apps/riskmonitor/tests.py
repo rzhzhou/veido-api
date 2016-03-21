c05ebf67b6295f6cd4d33c42c7ef4ebdca4f6e4a
lxwu <lxwu@shendu.info>
2016-03-21 05:07:25 -0400

    test cases

--- observer/apps/riskmonitor/tests.py
+++ observer/apps/riskmonitor/tests.py
@@ -11,10 +11,21 @@ class APITest(BaseTestCase):
         response = self.client.get('/api/industries')
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
+    def test_industry_list_view(self):
+        response = self.client.get('/api/industries')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_industry_detail_view(self):
+        response = self.client.get('/api/industries/1')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
     def test_news_list_view(self):
         response = self.client.get('/api/news')
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
     def test_news_detail_view(self):
         response = self.client.get('/api/news/1')
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        if response.status_code == 200:
+            self.assertEqual(response.status_code, status.HTTP_200_OK)
+        else:
+            self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
