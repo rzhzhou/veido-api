c169708123601608f12a137f8654308f168252a4
sli <sli@shendu.info>
2016-03-31 03:05:09 -0400

    fix test databases

--- observer/apps/riskmonitor/jobs/minutely/industry_make_score.py
+++ observer/apps/riskmonitor/jobs/minutely/industry_make_score.py
@@ -17,11 +17,8 @@ from django.core.exceptions import ObjectDoesNotExist
 from django_extensions.management.jobs import BaseJob
 
 from observer.apps.riskmonitor.models import (Industry, RiskNews,
-                                              ScoreIndustry, UserIndustry)
-
-
-
-
+                                              ScoreIndustry, UserIndustry,
+                                              UserArea)
 
 
 class Job(BaseJob):
@@ -31,23 +28,24 @@ class Job(BaseJob):
         self.reduce_score = 1
         self.user = 'changzhou'
         self.cycle = 90
-        self.time = datetime.today() - timedelta(days=0)
+        self.time = datetime.today() - timedelta(days=4)
 
-    def reducescore(self, industry):
+    def reducescore(self, industry, area):
         tz = pytz.timezone(settings.TIME_ZONE)
         end = tz.localize(self.time).replace(hour=23, minute=59, second=59, microsecond=0)
         start = end.replace(hour=0, minute=0, second=0, microsecond=0)
-        risknews = RiskNews.objects.filter(pubtime__range=(start, end), industry=industry)
+        risknews = RiskNews.objects.filter(pubtime__range=(start, end), industry=industry,
+                                            area=area)
         reducescore = risknews.count() * self.reduce_score
         return reducescore
 
-    # def getusers(self):
-    #     users = User.objects.all()
-    #     UserIndustry.objects.get(user=user).industry
-
-    def industry_make_score(self):
-        # user = User.objects.get(username=self.user)
-        industrys = Industry.objects.all()
+    def industry_make_score(self, user):
+        try:
+            area = UserArea.objects.get(user=user).area
+            user_industrys = UserIndustry.objects.filter(user=user)
+            industrys = [i.industry for i in user_industrys]
+        except ObjectDoesNotExist:
+            industrys = []
 
         upresult = ScoreIndustry.objects.last()
         for i in industrys:
@@ -64,15 +62,21 @@ class Job(BaseJob):
                 ScoreIndustry(score=(int(upscore) - self.reducescore(i) +
                     addscore), increment=increment, reducescore=self.reducescore(i),
                     industry=i,
-                    pubtime=self.time
+                    pubtime=self.time,
+                    user=user
                     ).save()
             else:
                 ScoreIndustry(score=100,
                                 industry=i,
-                                pubtime=self.time
+                                pubtime=self.time,
+                                user=user
                                 ).save()
 
-    def execute(self):
-        pass
 
-Job().industry_make_score()
+    def execute(self):
+        users = User.objects.all()
+        for u in users:
+            self.industry_make_score(u)
+
+
+Job().execute()
