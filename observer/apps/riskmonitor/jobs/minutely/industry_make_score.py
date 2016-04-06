2ad009ad31f73a53e971098b9f283d59be1af6c4
mfwu <mfwu@shendu.info>
2016-04-06 16:26:49 +0800

    add lower level area in the queryset

--- observer/apps/riskmonitor/jobs/minutely/industry_make_score.py
+++ observer/apps/riskmonitor/jobs/minutely/industry_make_score.py
@@ -1,26 +1,27 @@
 import os
 import sys
+import django
+
+reload(sys)
+sys.path.append('/home/feng/Project/observer/api/')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE",
+                      "observer.settings.development")
+django.setup()
+
 from datetime import datetime, timedelta
 
-import django
 import pytz
 from dateutil.relativedelta import relativedelta
 from django.conf import settings
 from django.contrib.auth.models import User
 from django.core.exceptions import ObjectDoesNotExist
+from django.db.models import Q
 from django_extensions.management.jobs import BaseJob
 
+from observer.apps.base.models import Area
 from observer.apps.riskmonitor.models import (Industry, RiskNews,
                                               ScoreIndustry, UserArea,
                                               UserIndustry)
-
-reload(sys)
-sys.path.append('/home/code/gitlab/test/api/')
-os.environ.setdefault("DJANGO_SETTINGS_MODULE",
-                      "observer.settings.development")
-django.setup()
-
-
 
 
 class Job(BaseJob):
@@ -32,17 +33,17 @@ class Job(BaseJob):
         self.cycle = 90
         self.time = datetime.today() - timedelta(days=1)
 
-    def reducescore(self, industry, area):
+    def reducescore(self, industry, areas):
         # tz = pytz.timezone(settings.TIME_ZONE)
         tz = pytz.timezone('UTC')
         end = tz.localize(self.time).replace(hour=23, minute=59, second=59, microsecond=0)
         start = end.replace(hour=0, minute=0, second=0, microsecond=0)
         risknews = RiskNews.objects.filter(pubtime__range=(start, end), industry=industry,
-                                            area=area)
+                                            area__in=areas)
         reducescore = risknews.count() * self.reduce_score
         return reducescore
 
-    def industry_make_score(self, user, area):
+    def industry_make_score(self, user, areas):
         try:
             user_industrys = UserIndustry.objects.filter(user=user)
             industrys = [i.industry for i in user_industrys]
@@ -59,8 +60,8 @@ class Job(BaseJob):
                                         increment - self.cycle),
                                         industry=i)
                 addscore = int(scoreindustry[0].reducescore) if scoreindustry else 0
-                ScoreIndustry(score=(int(upscore) - self.reducescore(i, area) +
-                    addscore), increment=increment, reducescore=self.reducescore(i, area),
+                ScoreIndustry(score=(int(upscore) - self.reducescore(i, areas) +
+                    addscore), increment=increment, reducescore=self.reducescore(i, areas),
                     industry=i,
                     pubtime=self.time,
                     user=user
@@ -72,11 +73,21 @@ class Job(BaseJob):
                                 user=user
                                 ).save()
 
+    def get_areas(self, area):
+        """
+        获取某个地域下所有地区(包括该地域)，地域为全国不适用于此方法
+        """
+        lower_level = Area.objects.filter(parent_id=area.id)
+        lower_id = map(lambda x: x.id, lower_level)
+        areas = Area.objects.filter(
+                Q(parent_id__in=lower_id) | Q(parent_id=area.id) | Q(id=area.id))
+        return areas
 
     def execute(self):
         users = UserArea.objects.all()
         for u in users:
-            self.industry_make_score(u.user, u.area)
+            areas = self.get_areas(u.area)
+            self.industry_make_score(u.user, areas)
 
 
 Job().execute()
