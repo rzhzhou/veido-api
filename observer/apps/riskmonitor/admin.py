3294b1e5d7fd4474ea1bef44a00bb6f9f391b8c5
mfwu <mfwu@shendu.info>
2016-05-16 09:18:30 +0800

    remove riskInspection

--- observer/apps/riskmonitor/admin.py
+++ observer/apps/riskmonitor/admin.py
@@ -7,18 +7,9 @@ from observer.apps.riskmonitor.models import (Brand, Enterprise, Industry,
                                               RiskData, RiskNews,
                                               ScoreEnterprise, ScoreIndustry,
                                               ScoreProduct, UserArea,
-                                              UserEnterprise, UserIndustry,
-                                              RiskInspection,RiskInspectionPublisher)
+                                              UserEnterprise, UserIndustry,)
 from import_export.admin import ImportExportActionModelAdmin
 from import_export.admin import ImportExportModelAdmin
-from resource import RiskInspectionResources
-
-
-class RiskInspectionAdmin(ImportExportModelAdmin):
-    resource_class = RiskInspectionResources
-    search_fields = ('url', 'title',)
-    list_display = ('title', 'qualitied', 'pubtime','publisher')
-    list_filter = ('pubtime', 'qualitied' )
 
 
 class IndustryAdmin(admin.ModelAdmin):
@@ -177,5 +168,3 @@ admin.site.register(ScoreEnterprise, ScoreEnterpriseAdmin)
 # admin.site.register(RiskData, RiskDataAdmin)
 admin.site.register(RiskNews, RiskNewsAdmin)
 admin.site.register(UserArea, UserAreaAdmin)
-admin.site.register(RiskInspectionPublisher)
-admin.site.register(RiskInspection, RiskInspectionAdmin)
