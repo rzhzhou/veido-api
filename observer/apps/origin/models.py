ef3c070741f4ad39a1d75f3d0c4de4aea097db99
lxwu <lxwu@shendu.info>
2016-06-28 17:34:03 +0800

    update admin

--- observer/apps/origin/models.py
+++ observer/apps/origin/models.py
@@ -5,7 +5,8 @@ from django.db import models
 class Area(models.Model):
     name = models.CharField(max_length=255, verbose_name=u'名称')
     level = models.BigIntegerField(null=False, verbose_name=u'等级')
-    parent = models.ForeignKey('self', null=True, blank=True, verbose_name=u'上一级')
+    parent = models.ForeignKey(
+        'self', null=True, blank=True, verbose_name=u'上一级')
 
     class Meta:
         app_label = 'origin'
@@ -72,14 +73,30 @@ class Inspection(models.Model):
     product = models.CharField(
         max_length=255, blank=True, null=True, verbose_name=u'产品种类')
 
-    area = models.ManyToManyField(Area, related_name='areas',
-                                  related_query_name='area', verbose_name=u'抽检地域')
-    industry = models.ManyToManyField(Industry, related_name='industries',
-                                      related_query_name='industry', verbose_name=u'行业')
-    enterprise_qualified = models.ManyToManyField(Enterprise, related_name='enterprises_qualified',
-                                        related_query_name='enterprise_qualified', verbose_name=u'合格企业')
-    enterprise_unqualified = models.ManyToManyField(Enterprise, related_name='enterprises_unqualified',
-                                        related_query_name='enterprise_unqualified', verbose_name=u'不合格企业')
+    area = models.ManyToManyField(
+        Area,
+        related_name='areas',
+        related_query_name='area',
+        verbose_name=u'抽检地域'
+    )
+    industry = models.ManyToManyField(
+        Industry,
+        # related_name='industries',
+        # related_query_name='industry',
+        verbose_name=u'行业'
+    )
+    enterprise_qualified = models.ManyToManyField(
+        Enterprise,
+        related_name='enterprises_qualified',
+        related_query_name='enterprise_qualified',
+        verbose_name=u'合格企业'
+    )
+    enterprise_unqualified = models.ManyToManyField(
+        Enterprise,
+        related_name='enterprises_unqualified',
+        related_query_name='enterprise_unqualified',
+        verbose_name=u'不合格企业'
+    )
 
     class Meta:
         app_label = 'origin'
