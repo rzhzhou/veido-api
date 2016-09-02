2684c3dc30b9730c35c939b2fc61cb255a66beff
sdu <sdu@shendu.info>
2016-09-01 23:27:12 -0700

    update enterprise.py

--- observer/apps/riskmonitor/service/enterprise.py
+++ observer/apps/riskmonitor/service/enterprise.py
@@ -1,7 +1,8 @@
 # -*- coding: utf-8 -*-
 from django.db.models import Count
 
-from observer.apps.riskmonitor.models import RiskNews, ScoreEnterprise, UserIndustry
+from observer.apps.origin.models import Inspection
+from observer.apps.riskmonitor.models import ScoreEnterprise, UserIndustry
 from observer.apps.riskmonitor.service.abstract import Abstract
 
 
@@ -11,18 +12,19 @@ class EnterpriseRank(Abstract):
         super(EnterpriseRank, self).__init__(params)
 
     def get_enterprises(self):
-        fields = ('enterprise__id', 'enterprise__name', 'enterprise__area__name')
+        fields = ('enterprise_unqualified__id', 'enterprise_unqualified__name',
+                  'enterprise_unqualified__area__name')
 
         cond = {
             'pubtime__gte': self.start,
             'pubtime__lt': self.end,
-            'enterprise__area__name': self.focus
+            'enterprise_unqualified__area__name': self.focus
         }
 
         # Exclude $cond None Value
         args = dict([(k, v) for k, v in cond.iteritems() if v is not None])
 
-        queryset = RiskNews.objects.filter(
-            **args).values_list(*fields).distinct().order_by('enterprise__id')
+        queryset = Inspection.objects.filter(
+            **args).values_list(*fields).distinct().order_by('enterprise_unqualified__id')
 
         return queryset
