d5a51094234141c02d6649ee734c4988e4687a0e
qwang <qwang@shendu.info>
2017-03-28 20:27:05 +0800

    improve

--- observer/apps/riskmonitor/views.py
+++ observer/apps/riskmonitor/views.py
@@ -720,7 +720,7 @@ class RiskNewsList(BaseView):
 
         limit = int(self.query_params.get('limit', 0))
 
-        queryset = NewsQuerySet(params=self.query_params).get_news_list()
+        queryset = NewsQuerySet(params=self.query_params).get_news_list().order_by('-pubtime')
 
         if limit:
             queryset = queryset[:limit]
