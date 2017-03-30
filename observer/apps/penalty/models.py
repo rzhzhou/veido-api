18dccb53dd6165d573027ea4170ac726c427df64
qwang <qwang@shendu.info>
2017-03-30 16:32:07 +0800

    improve penalty and inspection

--- observer/apps/penalty/models.py
+++ observer/apps/penalty/models.py
@@ -8,23 +8,25 @@ class AdministrativePenalties(models.Model):
     title = models.CharField(max_length=255, blank=True, verbose_name=u'标题')
     url = models.URLField(verbose_name=u'网站链接')
     content = HTMLField(blank=True, verbose_name=u'正文')
-    pubtime = models.DateTimeField(auto_now=False, verbose_name=u'发布时间')
+    pubtime = models.DateTimeField(
+        blank=True, null=True, auto_now=False, verbose_name=u'发布时间')
     publisher = models.CharField(max_length=255, verbose_name=u'发布者')
-    caseName = models.CharField(max_length=255, null=True, blank=True, verbose_name=u'案件名称')
-    illegalBehavior = models.CharField(max_length=255, null=True, blank=True, verbose_name=u'违法行为')
-    punishmentBasis = models.CharField(max_length=255, null=True, blank=True, verbose_name=u'处罚依据')
-    punishmentRsult = models.CharField(max_length=255, null=True, blank=True, verbose_name=u'处罚结果')
+    case_name = models.CharField(max_length=255, null=True, blank=True, verbose_name=u'案件名称')
+    illegal_behavior = models.CharField(max_length=255, null=True, blank=True, verbose_name=u'违法行为')
+    punishment_basis = models.CharField(max_length=255, null=True, blank=True, verbose_name=u'处罚依据')
+    punishment_result = models.CharField(max_length=255, null=True, blank=True, verbose_name=u'处罚结果')
 
-    area=models.ForeignKey(
-        'origin.Area', null=True, blank=True, verbose_name=u'地域')
+    area=models.ManyToManyField(
+        'origin.Area',
+        null=True, blank=True, verbose_name=u'地域')
 
-    enterprise=models.ForeignKey(
+    enterprise=models.ManyToManyField(
         'origin.Enterprise', null=True, blank=True, verbose_name=u'处罚企业')
 
-    industry=models.ForeignKey(
+    industry=models.ManyToManyField(
         'origin.Industry', null=True, blank=True, verbose_name=u'行业')
 
-    inspectionPublisher=models.ForeignKey(
+    inspection_publisher=models.ForeignKey(
         'origin.InspectionPublisher', null=True, blank=True, verbose_name=u'处罚机关')
 
     class Meta:
