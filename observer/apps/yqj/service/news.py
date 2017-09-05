f9d9cdaf32d65f954b3f8ddf71452961cd2c0643
mchen <mchen@shendu.info>
2017-09-05 19:12:20 +0800

    优化

--- observer/apps/yqj/service/news.py
+++ observer/apps/yqj/service/news.py
@@ -23,22 +23,22 @@ class NewsQuerySet(Abstract):
             'pubtime__lt': self.endtime,
         }
 
-        # queryset =''
+        queryset =''
 
-        # if category_id != 'null' and category_id is not None:
-        #     # args['category__id'] = category_id
-        #     category = Category.objects.get(pk =category_id)
-        #     queryset = category.articles.filter(**args).values(*fields)
-        #     # queryset = Article.categorys
-        # elif area_id != 'null' and area_id is not None:
-        #     args['area'] =Area.objects.filter(pk =area_id)
-        #     queryset = Article.objects.filter(**args).values(*fields)
-        # else:
-        #     args['category__name'] ='质监热点'
-        #     queryset = Article.objects.filter(**args).values(*fields)
+        if category_id != 'null' and category_id is not None:
+            # args['category__id'] = category_id
+            category = Category.objects.get(pk =category_id)
+            queryset = category.articles.filter(**args).values(*fields)
+            # queryset = Article.categorys
+        elif area_id != 'null' and area_id is not None:
+            args['area'] =Area.objects.filter(pk =area_id)
+            queryset = Article.objects.filter(**args).values(*fields)
+        else:
+            args['category__name'] ='质监热点'
+            queryset = Article.objects.filter(**args).values(*fields)
         
-        category = CategoryTwo.objects.get(name='信息参考')
-        queryset = category.articletwos.filter(**args).values(*fields)
+        # category = CategoryTwo.objects.get(name='信息参考')
+        # queryset = category.articletwos.filter(**args).values(*fields)
 
         return queryset
         
