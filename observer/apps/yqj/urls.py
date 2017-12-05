00150064277bace70e4b02f7414788872b3d3fb3
Sd <sdu@shendu.info>
2017-12-05 10:12:59 +0800

    improve yqj urls

--- observer/apps/yqj/urls.py
+++ observer/apps/yqj/urls.py
@@ -1,17 +1,18 @@
-from django.conf.urls import url
+from django.urls import path
 
-from observer.apps.yqj.views import (NewsView, EventView,ReferenceView,InsightView,RiskView,
-	                                 InspectionView,CategoryView,HomeView)
+from observer.apps.yqj.views import (NewsView, EventView, ReferenceView,
+                                     InsightView, RiskView, InspectionView,
+                                     CategoryView, HomeView)
 
 
 urlpatterns = [
-    url(r'^article$', NewsView.as_view()),#质监热点
-    url(r'^event$', EventView.as_view()),#质量事件
-    url(r'^reference$', ReferenceView.as_view()),#信息参考
-    url(r'^insight$', ReferenceView.as_view()),#专家视点
-    url(r'^risk$', RiskView.as_view()),#风险快讯
-    url(r'^inspection$', InspectionView.as_view()),#抽检信息
-    url(r'^category/(?P<id>\d+)$', CategoryView.as_view()),#业务信息
-    url(r'^area/(?P<id>\d+)$', CategoryView.as_view()),#区域状况
-    url(r'^$', HomeView.as_view()),#主页
+    path('dashboard', HomeView.as_view()),  # 主页
+    path('article', NewsView.as_view()),  # 质监热点
+    path('event', EventView.as_view()),  # 质量事件
+    path('reference', ReferenceView.as_view()),  # 信息参考
+    path('insight', ReferenceView.as_view()),  # 专家视点
+    path('risk', RiskView.as_view()),  # 风险快讯
+    path('inspection', InspectionView.as_view()),  # 抽检信息
+    path('category/<int:id>', CategoryView.as_view()),  # 业务信息
+    path('area/<int:id>', CategoryView.as_view()),  # 区域状况
 ]
