622732338975a8cbe5f11c3f28ad44ad54fa1c28
Sd <sdu@shendu.info>
2017-12-06 11:01:23 +0800

    update models(add level in inspection)

--- observer/apps/base/models.py
+++ observer/apps/base/models.py
@@ -59,8 +59,8 @@ class Industry(models.Model):
 
 
 class Corpus(models.Model):
-    riskword = models.TextField(max_length=255, default='', verbose_name='风险语料词')
-    invalidword = models.TextField(max_length=255, default='', verbose_name='无效词')
+    riskword = models.TextField(max_length=255, verbose_name='风险语料词')
+    invalidword = models.TextField(max_length=255, verbose_name='无效词')
 
     industry = models.ForeignKey(
         Industry,
@@ -80,11 +80,12 @@ class Inspection(models.Model):
     guid = models.CharField(max_length=32, primary_key=True, verbose_name='主键')
     title = models.CharField(max_length=255, verbose_name='标题')
     url = models.URLField(blank=True, null=True, verbose_name='网站链接')
-    content = models.TextField(default='', verbose_name='正文')
+    content = models.TextField(verbose_name='正文')
     pubtime = models.DateTimeField(blank=True, null=True, verbose_name='发布时间')
-    unitem = models.TextField(default='', verbose_name='不合格项')
-    brand = models.CharField(max_length=255, default='', verbose_name='商标')
-    product = models.CharField(max_length=255, default='', verbose_name='产品种类')
+    level = models.IntegerField(default=0, verbose_name='检验等级') #0, 默认值; 1 国抽; 2 省抽; 3 市抽;
+    unitem = models.TextField(verbose_name='不合格项')
+    brand = models.CharField(max_length=255, verbose_name='商标')
+    product = models.CharField(max_length=255, verbose_name='产品种类')
     source = models.CharField(max_length=255, verbose_name='信息来源')
     status = models.IntegerField(default=0, verbose_name='状态') # 0, 默认值 -1, 无效 1 有效
 
@@ -109,15 +110,15 @@ class AdministrativePenalties(models.Model):
     url = models.URLField(verbose_name='网站链接')
     pubtime = models.DateTimeField(blank=True, null=True, auto_now=False, verbose_name='发布时间')
     publisher = models.CharField(max_length=255, verbose_name='发布者')
-    case_name = models.CharField(max_length=255, default='', verbose_name='案件名称')
-    illegal_behavior = models.CharField(max_length=255, default='', verbose_name='违法行为')
-    punishment_basis = models.CharField(max_length=255, default='', verbose_name='处罚依据')
-    punishment_result = models.CharField(max_length=255, default='', verbose_name='处罚结果')
+    case_name = models.CharField(max_length=255, verbose_name='案件名称')
+    illegal_behavior = models.CharField(max_length=255, verbose_name='违法行为')
+    punishment_basis = models.CharField(max_length=255, verbose_name='处罚依据')
+    punishment_result = models.CharField(max_length=255, verbose_name='处罚结果')
     penalty_organ = models.CharField(max_length=255, verbose_name='处罚机关')
-    credit_code = models.CharField(max_length=255, default='', verbose_name='统一社会信用代码')
-    area = models.CharField(max_length=255, default='', verbose_name='地域')
-    enterprise = models.CharField(max_length=255, default='', verbose_name='处罚企业')
-    industry = models.CharField(max_length=255, default='', verbose_name='行业')
+    credit_code = models.CharField(max_length=255, verbose_name='统一社会信用代码')
+    area = models.CharField(max_length=255, verbose_name='地域')
+    enterprise = models.CharField(max_length=255, verbose_name='处罚企业')
+    industry = models.CharField(max_length=255, verbose_name='行业')
 
     class Meta:
         app_label = 'base'
@@ -150,7 +151,7 @@ class Article(models.Model):
     guid = models.CharField(max_length=32, primary_key=True, verbose_name='主键')
     title = models.CharField(max_length=255, blank=True, verbose_name='标题')
     url = models.URLField(verbose_name='网站链接')
-    content = models.TextField(default='', verbose_name='正文')
+    content = models.TextField(blank=True, verbose_name='正文')
     pubtime = models.DateTimeField(auto_now=False, verbose_name='发布时间')
     source = models.CharField(max_length=255, blank=True, verbose_name='信息来源')
     reprinted = models.IntegerField(verbose_name='转载数')
