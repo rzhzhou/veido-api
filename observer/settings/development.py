8b82fd7d6e8806be50d8987b872f164997c16ebb
Sd <sdu@shendu.info>
2017-12-08 15:19:06 +0800

    update authentication

--- observer/settings/development.py
+++ observer/settings/development.py
@@ -125,7 +125,7 @@ REST_FRAMEWORK = {
     ),
     'DEFAULT_AUTHENTICATION_CLASSES': (
         # 'rest_framework.authentication.SessionAuthentication',
-        'rest_framework.authentication.BasicAuthentication',
+        # 'rest_framework.authentication.BasicAuthentication',
         'rest_framework_jwt.authentication.JSONWebTokenAuthentication',
     ),
     'TEST_REQUEST_DEFAULT_FORMAT': 'json',
