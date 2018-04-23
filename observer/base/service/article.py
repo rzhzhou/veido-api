6e01534c57fd54dc9f53ec20f4ba50e24a5e4724
Sen Du <sdu@shendu.info>
2018-04-23 18:00:13 +0800

    update api

--- observer/base/service/article.py
+++ observer/base/service/article.py
@@ -1,7 +1,3 @@
-import xlrd
-
-from django.core.files.storage import default_storage
-from django.core.files.base import ContentFile
 from django.core.exceptions import ObjectDoesNotExist
 from django.db.models import Count, Q, F
 
@@ -10,6 +6,7 @@ from observer.base.models import(Article, ArticleArea, Category,
 from observer.base.service.abstract import Abstract
 from observer.utils.date_format import date_format
 from observer.utils.str_format import str_to_md5str
+from observer.utils.excel import (read, )
 
 
 class ArticleData(Abstract):
@@ -198,10 +195,73 @@ class RiskDataUpload(Abstract):
         self.user = user
 
     def upload(self, filename, file_obj):
-        default_storage.save('files/{0}.xlsx'.format(filename), ContentFile(file_obj.read()))
-        # wb = xlrd.open_workbook(filename=None, file_contents=file_obj.read())
-        # table = wb.sheets()[0]
-        # row = table.nrows
-        # print(row)
+        title = {'GUID': 0, 
+            '标题': 0, 
+            'URL': 0, 
+            '发布时间': 0, 
+            '发布媒体': 0, 
+            '风险程度': 0, 
+            '地域': 0, 
+            '类别': 0, }
 
-        return 200
+        data = read(file_contents=file_obj.read(), title=title)
+        total = len(data)
+        dupli = 0
+
+        for i, d in enumerate(data):
+            try:
+                title = d['标题']
+                url = d['URL']
+                pubtime = d['发布时间']
+                source = d['发布媒体']
+                score = d['风险程度']
+                area = d['地域']
+                category = d['类别']
+
+                a_guid = str_to_md5str(url)
+
+                if Article.objects.filter(guid=a_guid).exists():
+                    dupli += 1
+                    continue
+
+                areas = area.split(' ')
+                a_ids = Area.objects.filter(name__in=areas).values_list('id', flat=True)
+                categories = category.split(' ')
+                c_ids = Category.objects.filter(name__in=categories).values_list('id', flat=True)
+                for a_id in a_ids:
+                    if not ArticleArea.objects.filter(article_id=a_guid, area_id=a_id).exists():
+                        ArticleArea(
+                            article_id=a_guid,
+                            area_id=a_id,
+                        ).save()
+
+                for c_id in c_ids:
+                    if not ArticleCategory.objects.filter(article_id=a_guid, category_id=c_id).exists():
+                        ArticleCategory(
+                            article_id=a_guid,
+                            category_id=c_id,
+                        ).save()
+                
+                Article(
+                    guid=a_guid,
+                    title=title,
+                    url=url,
+                    pubtime=pubtime,
+                    source=source,
+                    score=score,
+                    risk_keyword='',
+                    invalid_keyword='',
+                    status=1,
+                ).save()
+
+            except Exception as e:
+                return {
+                    'status': 0, 
+                    'message': '操作失败！Excel %s 行存在问题，错误信息：%s！' % (i + 1, e)
+                }
+
+        return {
+                    'status': 1, 
+                    'message': '操作成功！共处理%s条数据，成功导入%s条数据，重复数据%s条！' % (total, total - dupli, dupli, )
+                }
+        
