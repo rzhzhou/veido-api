4c3abc1beb9888cd426ef84dcad5855e889336a1
Liuxi Wu <lxwu@shendu.info>
2018-07-06 11:18:43 +0800

    alter major pk

--- observer/base/models.py
+++ observer/base/models.py
@@ -139,7 +139,8 @@ class ConsumerIndustry(models.Model):
 
 
 class MajorIndustry(models.Model):
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False, verbose_name='行业编号')
+    # id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False, verbose_name='行业编号')
+    id = models.IntegerField(primary_key=True, editable=True, verbose_name='行业编号')
     name = models.CharField(max_length=100, verbose_name='行业名称')
     level = models.IntegerField(verbose_name='行业等级')
     desc = models.CharField(
