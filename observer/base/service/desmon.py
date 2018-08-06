1a0d1b57810a2d514fc73b90aca3ae7c8b9590e6
Zcheng <zcheng@shendu.info>
2018-08-06 15:45:17 +0800

    fixed bug

--- observer/base/service/desmon.py
+++ observer/base/service/desmon.py
@@ -3,7 +3,7 @@ from datetime import datetime
 from django.core.exceptions import ObjectDoesNotExist
 from django.db.models import Count, Q, F
 
-from observer.base.models import (DMLink, Corpus, )
+from observer.base.models import (DMLink, Corpus )
 from observer.base.service.abstract import Abstract
 from observer.utils.date_format import date_format
 
